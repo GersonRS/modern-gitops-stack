ff5a6844b80670937ba8dd8665d71beabdccf10f
GersonRS <gersonrodriguessantos8@gmail.com>
2024-03-17 01:44:33 -0300

    Fix: try adjust the somethings

--- locals.tf
+++ locals.tf
@@ -4,6 +4,6 @@ locals {
   base_domain            = format("%s.nip.io", replace(module.traefik.external_ip, ".", "-"))
   subdomain              = "apps"
   cluster_issuer         = module.cert-manager.cluster_issuers.ca
-  enable_service_monitor = false # Can be enabled after the first bootstrap.
+  enable_service_monitor = true # Can be enabled after the first bootstrap.
   app_autosync           = { allow_empty = false, prune = true, self_heal = true }
 }
